5d48be817edf17bdb9c1e3af6c311e761ed15bc0
Marcela B Petroli <marcelabpetroli@gmail.com>
2023-01-16 19:16:40 +0100

    Delete console.log

--- src/js/7_server.js
+++ src/js/7_server.js
@@ -1,21 +1,20 @@
-"use strict";
+'use strict';
 
 function sendRequest() {
-  fetch("https://awesome-profile-cards.herokuapp.com/card/", {
-    method: "POST",
+  fetch('https://awesome-profile-cards.herokuapp.com/card/', {
+    method: 'POST',
     body: JSON.stringify(data),
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
   })
     .then((response) => response.json())
     .then((response) => {
-      console.log(response.json);
       if (response.success) {
-        linkShare.innerHTML = "<a class='card-created-url' href=" + response.cardURL + ">" + response.cardURL + "</a>";
-        linkTwitter.href = `https://twitter.com/intent/tweet?text=%C2%A1Mira%20la%20m%C3%A1gica%20tarjeta%20que%20he%20creado!!&url=${response.cardURL }`;
+        linkShare.innerHTML = "<a class='card-created-url' href=" + response.cardURL + '>' + response.cardURL + '</a>';
+        linkTwitter.href = `https://twitter.com/intent/tweet?text=%C2%A1Mira%20la%20m%C3%A1gica%20tarjeta%20que%20he%20creado!!&url=${response.cardURL}`;
       } else {
-        linkShare.innerHTML = "ERROR:" + response.error;
+        linkShare.innerHTML = 'ERROR:' + response.error;
       }
     });
 }
